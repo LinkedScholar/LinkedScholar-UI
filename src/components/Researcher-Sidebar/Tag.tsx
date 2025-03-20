4cea7778a508955dc534d5bfc3ec733d95417348
mtiessler <max.tiessler@upc.edu>
2025-03-20 18:32:56 +0100

    added tags and sidebar does not show empty things

--- src/components/Researcher-Sidebar/Tag.tsx
+++ src/components/Researcher-Sidebar/Tag.tsx
@@ -1,38 +1,11 @@
 import React from "react";
 import "../../styles/components/tag.scss";
-
 interface TagProps {
-  text: string;
-  onClick?: () => void;
-  onAdd?: () => void;
-  isInteractive?: boolean;
+  label: string;
 }
 
-const Tag: React.FC<TagProps> = ({ 
-  text, 
-  onClick, 
-  onAdd,
-  isInteractive = false 
-}) => {
-  return (
-    <div 
-      className={`tag ${isInteractive ? 'interactive' : ''}`} 
-      onClick={onClick}
-    >
-      <span className="tag-text">{text}</span>
-      {onAdd && (
-        <button 
-          className="tag-add-button"
-          onClick={(e) => {
-            e.stopPropagation();
-            onAdd();
-          }}
-        >
-          <i className="mdi mdi-plus"></i>
-        </button>
-      )}
-    </div>
-  );
+const Tag: React.FC<TagProps> = ({ label }) => {
+  return <span className="tag">{label}</span>;
 };
 
 export default Tag;
